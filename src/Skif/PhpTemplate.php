a1b3d7605f9d33158823b10fe4d992cdf1d777ad
skulkov <sergey.kulkov@gmail.com>
2015-09-17 18:03:38 +0300

    kss

--- src/Skif/PhpTemplate.php
+++ src/Skif/PhpTemplate.php
@@ -3,11 +3,6 @@ namespace Skif;
 
 class PhpTemplate
 {
-    public static function getSkifTemplatePath()
-    {
-        return dirname(dirname(__DIR__));
-    }
-
     public static function renderSkifTemplate($template_file, $variables = array()) {
         extract($variables, EXTR_SKIP);
         ob_start();
@@ -21,20 +16,20 @@ class PhpTemplate
     }
 
     public static function renderTemplate($template_file, $variables = array()) {
-        $relative_to_root_site_file_path = \Skif\Path::getRootSitePath() . DIRECTORY_SEPARATOR . $template_file;
+        $relative_to_root_site_file_path = \Skif\Path::getSiteViewsPath() . DIRECTORY_SEPARATOR . $template_file;
 
         if (file_exists($relative_to_root_site_file_path)) {
             return \Skif\PhpTemplate::renderTemplateRelativeToRootSitePath($template_file, $variables);
         }
 
-        return \Skif\PhpTemplate::renderSkifTemplate($template_file, $variables);
+        return \Skif\PhpTemplate::renderTemplate($template_file, $variables);
     }
 
     public static function renderTemplateRelativeToRootSitePath($template_file, $variables = array()) {
         extract($variables, EXTR_SKIP);
         ob_start();
 
-        require \Skif\Path::getRootSitePath() . DIRECTORY_SEPARATOR . $template_file;
+        require \Skif\Path::getSiteViewsPath() . DIRECTORY_SEPARATOR . $template_file;
 
         $contents = ob_get_contents();
         ob_end_clean();
@@ -44,7 +39,7 @@ class PhpTemplate
 
     public static function renderTemplateBySkifModule($module, $template_file, $variables = array()) {
         if (\Skif\PhpTemplate::existsTemplateByModuleRelativeToRootSitePath($module, $template_file)) {
-            $relative_to_root_site_file_path = \Skif\Path::VIEWS_DIR_NAME . DIRECTORY_SEPARATOR . 'Skif' . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . $template_file;
+            $relative_to_root_site_file_path = 'Skif' . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . $template_file;
             return \Skif\PhpTemplate::renderTemplateRelativeToRootSitePath($relative_to_root_site_file_path, $variables);
         }
 
