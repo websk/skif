c7c1b52a9ffd85bd4fa37919ede32261b5979766
skulkov <sergey.kulkov@gmail.com>
2015-10-14 22:22:01 +0300

    kss

--- src/Skif/Content/views/content_news_last.tpl.php
+++ src/Skif/Content/views/content_news_last.tpl.php
@@ -13,7 +13,16 @@ foreach ($contents_ids_arr as $content_id) {
         <div class="news_title">
             <a href="<?php echo $content_obj->getUrl(); ?>"><?php echo $content_obj->getTitle(); ?></a>
         </div>
-        <div class="news_image"><img src="<?php echo \Skif\Image\ImageManager::getImgUrlByPreset($content_obj->getImagePath(), '200_auto'); ?>" alt="<?php $content_obj->getTitle() ?>" title="<?php echo $content_obj->getTitle() ?>" class="img-responsive"></div>
+        <?php
+        if ($content_obj->getImage()) {
+            ?>
+            <div class="news_image"><img
+                    src="<?php echo \Skif\Image\ImageManager::getImgUrlByPreset($content_obj->getImagePath(), '200_auto'); ?>"
+                    alt="<?php $content_obj->getTitle() ?>" title="<?php echo $content_obj->getTitle() ?>"
+                    class="img-responsive"></div>
+            <?php
+        }
+        ?>
         <div><?= \Skif\Content\ContentUtils::filterContent($content_obj->getAnnotation()) ?></div>
     </div>
 <?php
