49c90562e22b71b7b519dc2ad62efdbe06d2b9ac
skulkov <sergey.kulkov@gmail.com>
2015-10-14 22:26:15 +0300

    kss

--- src/Skif/Content/views/content_news_last.tpl.php
+++ src/Skif/Content/views/content_news_last.tpl.php
@@ -19,7 +19,7 @@ foreach ($contents_ids_arr as $content_id) {
             <div class="news_image"><img
                     src="<?php echo \Skif\Image\ImageManager::getImgUrlByPreset($content_obj->getImagePath(), '200_auto'); ?>"
                     alt="<?php $content_obj->getTitle() ?>" title="<?php echo $content_obj->getTitle() ?>"
-                    class="img-responsive"></div>
+                    class="img-responsive img-thumbnail"></div>
             <?php
         }
         ?>
