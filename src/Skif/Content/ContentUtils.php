15d0a8b9240d194e6e3fee81e552324f14a3c32e
skulkov <sergey.kulkov@gmail.com>
2015-11-10 17:01:39 +0300

    kss

--- src/Skif/Content/ContentUtils.php
+++ src/Skif/Content/ContentUtils.php
@@ -118,6 +118,22 @@ class ContentUtils
 
     public static function filterContent($content)
     {
-        return strip_tags($content, "<p><b><strong><em><span><br><div><a><img><h1><h2><h3><h4><table><tr><td><tbody><thead><th><li><ul><ol><script><hr><form><input><iframe>");
+        $allowable_tags_arr = array(
+            '<p>',
+            '<b><strong><em><i>',
+            '<span>',
+            '<br>',
+            '<div>',
+            '<a>',
+            '<img>',
+            '<h1><h2><h3><h4>',
+            '<table><tr><td><tbody><thead><th>',
+            '<li><ul><ol>',
+            '<script>',
+            '<hr>',
+            '<form><input><iframe>'
+        );
+
+        return strip_tags($content, implode('', $allowable_tags_arr));
     }
 }
