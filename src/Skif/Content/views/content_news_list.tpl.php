560b6d5f6670f6928a337af7e6086995e00f179c
skulkov <sergey.kulkov@gmail.com>
2015-11-12 18:58:37 +0300

    kss

--- src/Skif/Content/views/content_news_list.tpl.php
+++ src/Skif/Content/views/content_news_list.tpl.php
@@ -25,26 +25,11 @@ foreach ($content_ids_arr as $content_id) {
         continue;
     }
     */
-    ?>
-    <div class="list_news">
-        <div class="news_data"><?= date('d.m.Y', $content_obj->getUnixTime()) ?></div>
-        <div class="news_title">
-            <a href="<?php echo $content_obj->getUrl(); ?>"><?php echo $content_obj->getTitle(); ?></a>
-        </div>
-        <div class="row">
-            <?php
-            $col = 12;
-            if ($content_obj->getImage()) {
-                $col = 9;
-                ?>
-                <div class="col-md-3 news_image"><img src="<?php echo \Skif\Image\ImageManager::getImgUrlByPreset($content_obj->getImagePath(), '120_auto'); ?>" alt="<?php $content_obj->getTitle() ?>" title="<?php echo $content_obj->getTitle() ?>" class="img-responsive"></div>
-            <?php
-            }
-            ?>
-            <div class="col-md-<?php echo $col; ?>"><?= \Skif\Content\ContentUtils::filterContent($content_obj->getAnnotation()) ?></div>
-        </div>
-    </div>
-<?php
+    echo \Skif\PhpTemplate::renderTemplateBySkifModule(
+        'Content',
+        'content_in_list.tpl.php',
+        array('content_id' => $content_id)
+    );
 }
 
 $count_all_articles = \Skif\Content\ContentUtils::getCountPublishedContentsByType($content_type);
