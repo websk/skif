16310a228dc8daf4ce239a6c6e7a91bbdd648fc4
skulkov <sergey.kulkov@gmail.com>
2015-11-13 17:08:41 +0300

    kss

--- src/Skif/Content/views/rubrics_list.tpl.php
+++ src/Skif/Content/views/rubrics_list.tpl.php
@@ -22,12 +22,13 @@ $content_type_obj = \Skif\Content\ContentType::factory($content_type_id);
             <tr>
                 <td>
                     <a href="<?php echo $rubric_obj->getEditorUrl(); ?>"><?php echo $rubric_obj->getName(); ?></a>
+                    <a href="<?php echo $rubric_obj->getEditorUrl(); ?>"><span class="glyphicon glyphicon-edit" title="Редактировать"></span></a>
                 </td>
                 <td>
                     <?php echo $rubric_obj->getComment(); ?>
                 </td>
                 <td align="right">
-                    <a href="<?php echo $rubric_obj->getEditorUrl(); ?>"><span class="glyphicon glyphicon-edit" title="Редактировать"></span></a>
+                    <a href="<?php echo $rubric_obj->getUrl(); ?>" target="_blank"><span class="glyphicon glyphicon-new-window"></span></a>
                     <a href="<?php echo $rubric_obj->getDeleteUrl(); ?>" onClick="return confirm('Вы уверены, что хотите удалить?')"><span class="glyphicon glyphicon-remove" title="Удалить"></span></a>
                 </td>
             </tr>
