6f3d04bc9d8a520f121963bccef57ba91f878119
skulkov <sergey.kulkov@gmail.com>
2015-11-19 19:41:54 +0300

    kss

--- views/layouts/layout.admin.tpl.php
+++ views/layouts/layout.admin.tpl.php
@@ -50,7 +50,7 @@ $user_obj = \Skif\Users\User::factory($user_id);
     <!-- Custom Fonts -->
     <link href="/vendor/bower/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <link rel="stylesheet" href="/vendor/websk/skif/assets/css/admin_new.css" type="text/css">
+    <link rel="stylesheet" href="/vendor/websk/skif/assets/css/admin.css" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
