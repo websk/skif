f871a320c97da0f18e830b583266f6e1fcdf4596
skulkov <sergey.kulkov@gmail.com>
2015-11-22 16:03:14 +0300

    kss

--- src/Skif/KeyValue/views/key_values_list.tpl.php
+++ src/Skif/KeyValue/views/key_values_list.tpl.php
@@ -4,27 +4,33 @@ $key_value_ids_arr = \Skif\KeyValue\KeyValueUtils::getKeyValueIdsArr();
 <p><a href="/admin/key_value/edit/new" class="btn btn-primary"><span class="glyphicon glyphicon-plus"></span> Добавить переменную</a></p>
 <p></p>
 
-<div class="table-responsive">
+<div>
     <table class="table table-striped table-hover">
         <colgroup>
-            <col class="col-md-4">
-            <col class="col-md-6">
-            <col class="col-md-2">
+            <col class="col-md-1 col-sm-1 col-xs-1">
+            <col class="col-md-4 col-sm-6 col-xs-6">
+            <col class="col-md-4 hidden-sm hidden-xs">
+            <col class="col-md-3 col-sm-5 col-xs-5">
         </colgroup>
     <?php
     foreach ($key_value_ids_arr as $key_value_id) {
         $key_value_obj = \Skif\KeyValue\KeyValue::factory($key_value_id);
         ?>
         <tr>
+            <td><?php echo $key_value_obj->getId(); ?></td>
             <td>
                 <a href="/admin/key_value/edit/<?php echo $key_value_id ?>"><?= $key_value_obj->getName() ?></a>
             </td>
-            <td>
-                <?= $key_value_obj->getDescription() ?>
+            <td class="hidden-sm hidden-xs">
+                <?php echo $key_value_obj->getDescription(); ?>
             </td>
             <td align="right">
-                <a href="/admin/key_value/edit/<?php echo $key_value_id; ?>"><span class="glyphicon glyphicon-edit text-warning" title="Редактировать"></span></a>
-                <a href="/admin/key_value/delete/<?php echo $key_value_id; ?>" onClick="return confirm('Вы уверены, что хотите удалить?')"><span class="glyphicon glyphicon-remove text-danger" title="Удалить"></span></a>
+                <a href="/admin/key_value/edit/<?php echo $key_value_id; ?>" title="Редактировать" class="btn btn-outline btn-default btn-sm">
+                    <span class="fa fa-edit fa-lg text-warning fa-fw"></span>
+                </a>
+                <a href="/admin/key_value/delete/<?php echo $key_value_id; ?>" onClick="return confirm('Вы уверены, что хотите удалить?')" title="Удалить" class="btn btn-outline btn-default btn-sm">
+                    <span class="fa fa-trash-o fa-lg text-danger fa-fw"></span>
+                </a>
             </td>
         </tr>
         <?php
