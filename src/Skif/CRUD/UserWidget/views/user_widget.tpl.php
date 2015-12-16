c5545caa81f4f596f9b1b7df3ac1f1f6c026171f
skulkov <sergey.kulkov@gmail.com>
2015-12-16 17:06:58 +0300

    kss

--- src/Skif/CRUD/UserWidget/views/user_widget.tpl.php
+++ src/Skif/CRUD/UserWidget/views/user_widget.tpl.php
@@ -8,7 +8,7 @@
 
 $users_ids_arr = \Skif\Users\UsersUtils::getUsersIdsArr($filtered_user_role_id);
 ?>
-<select id="<?php echo $field_name; ?>" name="<?php echo $field_name; ?>" class="form-control"<?php echo !$disabled ? ' readonly' : '' ?>>
+<select id="<?php echo $field_name; ?>" name="<?php echo $field_name; ?>" class="form-control"<?php echo !$disabled ? ' disabled' : '' ?>>
     <?php
     foreach ($users_ids_arr as $user_id) {
         $user_obj = \Skif\Users\User::factory($user_id);
