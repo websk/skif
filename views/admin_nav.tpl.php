9540da85043f4b883d0128057e8fd0489ec98de0
skulkov <sergey.kulkov@gmail.com>
2016-01-14 14:06:27 +0300

    kss

--- views/admin_nav.tpl.php
+++ views/admin_nav.tpl.php
@@ -10,16 +10,33 @@ if (!isset($admin_nav_arr)) {
 if (!\Skif\Users\AuthUtils::currentUserIsAdmin()) {
     return;
 }
-
-foreach ($admin_nav_arr as $admin_nav_link => $admin_nav_title) {
-    ?>
-    <div class="navbar navbar-default">
-        <ul class="nav navbar-nav">
-            <li>
-                <a href="<?php echo $admin_nav_link; ?>" target="_blank"><?php echo $admin_nav_title; ?>&nbsp;<sup><span
-                            class="glyphicon glyphicon-new-window"></span></sup></a>
-            </li>
-        </ul>
+?>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#tour-edit-navbar-collapse" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><span class="fa fa-wrench"></span></a>
+        </div>
+        <div class="collapse navbar-collapse" id="tour-edit-navbar-collapse">
+            <ul class="nav navbar-nav">
+                <?php
+                foreach ($admin_nav_arr as $admin_nav_link => $admin_nav_title) {
+                    ?>
+                    <li>
+                        <a href="<?php echo $admin_nav_link; ?>" target="_blank"><?php echo $admin_nav_title; ?>
+                            &nbsp;<sup><span
+                                    class="glyphicon glyphicon-new-window"></span></sup></a>
+                    </li>
+                    <?php
+                }
+                ?>
+            </ul>
+        </div>
     </div>
-<?php
-}
+</nav>
+
