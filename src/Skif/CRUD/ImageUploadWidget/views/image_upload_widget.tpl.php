386b8e0cdc5d5d9234c49007f38bf07aaa8ad195
skulkov <sergey.kulkov@gmail.com>
2016-01-15 17:26:07 +0300

    kss

--- src/Skif/CRUD/ImageUploadWidget/views/image_upload_widget.tpl.php
+++ src/Skif/CRUD/ImageUploadWidget/views/image_upload_widget.tpl.php
@@ -6,147 +6,96 @@
  */
 ?>
 
+<span class="btn btn-success fileinput-button">
+        <i class="glyphicon glyphicon-plus"></i>
+        <span>Выберите файлы...</span>
+        <input id="tour_photo" type="file" name="tour_photo[]" multiple>
+    </span>
+<br>
+<br>
+
+<div id="files" class="files"></div>
+
+<link rel="stylesheet" href="<?php echo $bower_path; ?>/blueimp-file-upload/css/jquery.fileupload.css">
+<script src="<?php echo $bower_path; ?>/blueimp-file-upload/js/jquery.fileupload.js"></script>
+<script src="<?php echo $bower_path; ?>/blueimp-file-upload/js/jquery.fileupload-process.js"></script>
+<script src="<?php echo $bower_path; ?>/blueimp-file-upload/js/jquery.fileupload-validate.js"></script>
+
 <script>
-    var skif = skif || {};
-
-    skif.admin = {};
-    skif.admin.utils = {};
-    skif.admin.utils.crud = {};
-
-    skif.admin.utils.crud.imageAjaxUploadWidget = function (formField) {
-        var currentParent = $("#imageUploadWidget_" + formField);
-        var fileInput = currentParent.find('.file-select');
-        var fileInputBtn = currentParent.find('.select-file-btn');
-
-        var uploadFiles = function (event) {
-            event.stopPropagation();
-            event.preventDefault();
-
-            var files = fileInput.prop('files'),
-                uploadButton = currentParent.find('.upload-button'),
-                data = new FormData(),
-                target_folder_select = currentParent.find('.target_folder'),
-                not_force_file_extension = currentParent.find('.not_force_file_extension');
-
-            uploadButton.text("Загружается...").attr('disabled', 'disabled');
-
-            $.each(files, function (key, value) {
-                data.append(key, value);
-            });
-
-            $.each(target_folder_select, function (key, value) {
-                data.append('target_folder', value.value);
-            });
-
-            $.each(not_force_file_extension, function (key, value) {
-                if ($(value).prop("checked") == true) {
-                    data.append('not_force_file_extension', value.value);
-                }
-            });
-
-            $.ajax({
-                url: '/images/upload_to_images',
-                type: 'POST',
-                data: data,
-                cache: false,
-                dataType: 'json',
-                processData: false,
-                contentType: false,
-                success: function (response) {
-                    currentParent.find('.uploaded-image').html('<div class="uploaded-image__preview">' +
-                    '    <img src="<?=\Skif\CRUD\ImageUploadWidget\ImageUploadWidget::getIconImagePresetFolder()?>/' + response.fileName + '" />' +
-                    '</div>');
-                    currentParent.find('.internal-file-name').val(response.fileName);
-                },
-                error: function (jqXHR, textStatus) {
-                    alert("Произошла ошибка, попробуйте загрузить еще раз");
-                },
-                complete: function () {
-                    uploadButton.text("Загрузить").removeAttr('disabled');
-                }
-            });
-        };
-
-        currentParent.on('click', '.upload-button', uploadFiles);
-
-        fileInputBtn.on('click', function(event) {
-            event.preventDefault();
-            fileInput.click();
+    function ajaxUpdateImageList() {
+        $.ajax({
+            url: '/tour/list_photo/<?php echo $tour_id; ?>',
+            success: function (html) {
+                $('#tour_photos_list').html(html);
+            }
         });
-    };
-</script>
-
-<?php
-
-$images_folder = '/files/images';
-
-$dirty_images_subdirs_arr = glob($images_folder . '/*', GLOB_ONLYDIR);
-$images_subdirs_arr = array();
-
-foreach ($dirty_images_subdirs_arr as $subdir) {
-
-    // проверяем, что путь начинается с папки картинок
-    if (mb_substr($subdir, 0, mb_strlen($images_folder)) != $images_folder) {
-        //continue;
     }
 
-    // вырезаем часть пути внутри папки картинок
-    $subdir_in_images = mb_substr($subdir, mb_strlen($images_folder) + mb_strlen(DIRECTORY_SEPARATOR));
-
-
-    $images_subdirs_arr[] = $subdir_in_images;
-}
-
-if (!empty($default_folder_name)) {
-    if (!in_array($default_folder_name , $images_subdirs_arr)) {
-        mkdir($_SERVER['DOCUMENT_ROOT'].'/'.$images_folder.'/'.$default_folder_name);
+    function ajaxDeleteImage(tour_photo_id) {
+        $.ajax({
+            type: "POST",
+            url: "/tour/delete_photo/" + tour_photo_id,
+            success: function () {
+                ajaxUpdateImageList();
+            }
+        });
     }
 
-    $images_subdirs_arr = array($default_folder_name);
-}
-?>
+    $(function () {
+        ajaxUpdateImageList();
 
-<div id="imageUploadWidget_<?php echo $field_name ?>">
-    <?php
-    if (count($images_subdirs_arr)) {
-        echo '<div>Загрузить в папку: <select name="target_folder" class="target_folder">';
+        var url = '/tour/add_photo/<?php echo $tour_id; ?>';
 
-        if (empty($default_folder_name)) {
-            echo '<option value="" selected>';
-        }
-
-        foreach ($images_subdirs_arr as $subdir) {
-            echo '<option value="' . $subdir . '">' . $subdir;
-        }
-
-        echo "</select></div>";
-    }
-    ?>
-
-    <div class="file-upload">
-        <div class="row">
-            <div class="col-sm-8">
-                <input type="file" class="file-select form-control" name="photo" />
-                <button type="submit" class="btn btn-default select-file-btn">Выберите файл</button>
-            </div>
-            <div class="col-sm-4">
-                <button type="submit" class="btn btn-default pull-right upload-button">Загрузить</button>
-            </div>
-        </div>
-    </div>
-
-    <div class="uploaded-image">
-    <?php if ($field_value) { ?>
-        <a class="uploaded-image__previewtop" target="_blank" href="<?php echo \Skif\Image\ImageManager::getImgUrlByPreset($field_value, \Skif\Image\ImagePresets::IMAGE_PRESET_UPLOAD); ?>">
-            <img src="<?php echo \Skif\CRUD\ImageUploadWidget\ImageUploadWidget::getIconFileUrlByFilename($field_value); ?>">
-        </a>
-    <?php } ?>
-    </div>
-    <input type="hidden" name="<?php echo $field_name ?>" class="internal-file-name" value="<?php echo $field_value ?>">
-</div>
-
-<script>
-    (function () {
-        skif.admin.utils.crud.imageAjaxUploadWidget("<?php echo $field_name ?>");
-    })();
+        $('#tour_photo').fileupload({
+            url: url,
+            dataType: 'json',
+            autoUpload: true,
+            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+            maxFileSize: 5242880,
+            previewThumbnail: false,
+            messages: {
+                maxNumberOfFiles: 'Превышено максимальное количество файлов, загружаемое за один раз',
+                acceptFileTypes: 'Этот файл не изображение',
+                maxFileSize: 'Размер файла слишком большой',
+                minFileSize: 'Размер файла слишком маленький'
+            }
+        }).on('fileuploadadd', function (e, data) {
+            data.context = $('<div/>').appendTo('#files');
+            $.each(data.files, function (index, file) {
+                var node = $('<p/>')
+                    .append($('<span/>').text(file.name));
+                node.appendTo(data.context);
+            });
+        }).on('fileuploadprocessalways', function (e, data) {
+            var index = data.index,
+                file = data.files[index],
+                node = $(data.context.children()[index]);
+            if (file.error) {
+                node
+                    .append('<br>')
+                    .append($('<span class="text-danger"/>').text(file.error));
+            }
+        }).on('fileuploaddone', function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                if (file.url) {
+                    $(data.context.children()[index])
+                        .append(' <div class="text-success">Файл  загружен</div>');
+                } else if (file.error) {
+                    var error = $('<span class="text-danger"/>').text(file.error);
+                    $(data.context.children()[index])
+                        .append('<br>')
+                        .append(error);
+                }
+            });
+            ajaxUpdateImageList();
+        }).on('fileuploadfail', function (e, data) {
+            $.each(data.files, function (index) {
+                var error = $('<span class="text-danger"/>').text('Файл не удалось загрузить.');
+                $(data.context.children()[index])
+                    .append('<br>')
+                    .append(error);
+            });
+        }).prop('disabled', !$.support.fileInput)
+            .parent().addClass($.support.fileInput ? undefined : 'disabled');
+    });
 </script>
