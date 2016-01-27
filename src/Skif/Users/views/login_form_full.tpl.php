873526bd570f2f216d83e8fcb1e497d83001b197
skulkov <sergey.kulkov@gmail.com>
2016-01-27 10:52:49 +0300

    kss

--- src/Skif/Users/views/login_form_full.tpl.php
+++ src/Skif/Users/views/login_form_full.tpl.php
@@ -1,19 +1,10 @@
 <?php
-/**
- * @var $destination
- */
-if (!isset($destination)) {
-    $destination = '/';
-}
 ?>
 
 <form action="/user/login" method="post">
     <div class="form-group">
         <label>Email</label>
-        <div class="input-group">
-            <span class="input-group-addon">@</span>
-            <input type="text" name="email" maxlength="30" class="form-control">
-        </div>
+        <input type="text" name="email" class="form-control">
     </div>
     <div class="form-group">
         <label >Пароль</label>
@@ -21,9 +12,9 @@ if (!isset($destination)) {
     </div>
     <div class="checkbox">
         <label>
-            <input type="checkbox" name="save_auth" value="1"> Запомнить
+            <input type="checkbox" name="save_auth" value="1"> Запомнить меня
         </label>
     </div>
-    <input type="hidden" name="destination" value="<?php echo $destination; ?>">
+    <input type="hidden" name="destination" value="/">
     <input type="submit" value="Войти" class="btn btn-default">
 </form>
