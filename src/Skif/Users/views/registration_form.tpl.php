f5c03174a3c940cbc4fe62d3031d5164a2655637
skulkov <sergey.kulkov@gmail.com>
2016-01-27 18:09:10 +0300

    kss

--- src/Skif/Users/views/registration_form.tpl.php
+++ src/Skif/Users/views/registration_form.tpl.php
@@ -34,7 +34,7 @@ $user_obj = new \Skif\Users\User();
     </div>
 
     <div class="form-group">
-        <div class="col-md-offset-2 col-md-10">
+        <div class="col-md-offset-4 col-md-8">
             <img src="<?php echo \Skif\Captcha\Captcha::getUrl(); ?>" border="0" alt="Введите этот защитный код">
             <input type="text" size="5" name="captcha" class="form-control">
             <span class="help-block">Введите код, изображенный на картинке</span>
