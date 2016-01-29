cb97265d83eba0da9f143e134d14248288d88f41
skulkov <sergey.kulkov@gmail.com>
2016-01-29 17:12:22 +0300

    kss

--- src/Skif/Users/views/registration_form.tpl.php
+++ src/Skif/Users/views/registration_form.tpl.php
@@ -51,6 +51,24 @@ $user_obj = new \Skif\Users\User();
 
 <script type="text/javascript">
     $().ready(function () {
+        $.validator.setDefaults({
+            highlight: function(element) {
+                $(element).closest('.form-group').addClass('has-error');
+            },
+            unhighlight: function(element) {
+                $(element).closest('.form-group').removeClass('has-error');
+            },
+            errorElement: 'span',
+            errorClass: 'help-block',
+            errorPlacement: function(error, element) {
+                if(element.parent('.input-group').length) {
+                    error.insertAfter(element.parent());
+                } else {
+                    error.insertAfter(element);
+                }
+            }
+        });
+
         $("#registration_form").validate({
             ignore: ":hidden",
             rules: {
