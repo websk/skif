d2b7d53d82a6deb415f433a8b1db3e9b2f8dec65
skulkov <sergey.kulkov@gmail.com>
2016-02-05 15:05:16 +0300

    kss

--- routing.php
+++ routing.php
@@ -145,7 +145,7 @@ if (strpos($current_url_no_query, '/admin') !== false) {
 \Skif\UrlManager::route('@^/images/upload$@', '\Skif\Image\ImageController', 'uploadAction');
 //\Skif\UrlManager::route('@^/images/upload_to_files$@', '\Skif\Image\ImageController', 'uploadToFilesAction');
 //\Skif\UrlManager::route('@^/images/upload_to_images$@', '\Skif\Image\ImageController', 'uploadToImagesAction');
-
+echo 1;
 \Skif\UrlManager::route('@^@', '\Skif\Content\ContentController', 'viewAction');
 \Skif\UrlManager::route('@^@', '\Skif\Content\RubricController', 'listAction');
 \Skif\UrlManager::route('@^/(.+)$@i', '\Skif\Content\ContentController', 'listAction');
