5e3c0bd1ce9ecaa128d405aebecea4e32e668a63
skulkov <sergey.kulkov@gmail.com>
2016-02-05 13:03:32 +0300

    kss

--- src/Skif/AdminController.php
+++ src/Skif/AdminController.php
@@ -12,22 +12,8 @@ class AdminController
             \Skif\Http::redirect('/admin/content/page');
         }
 
-        $layout = \Skif\Conf\ConfWrapper::value('layout.admin');
-        if (array_key_exists('noheader', $_REQUEST)) {
-            $layout = \Skif\Conf\ConfWrapper::value('layout.empty');
-        }
-
-        $content = '<h2>Вход в систему управления</h2>';
-        $content .= \Skif\PhpTemplate::renderTemplateBySkifModule('Users', 'login_form.tpl.php', array('destination' => '/admin'));
-
         echo \Skif\PhpTemplate::renderTemplate(
-            $layout,
-            array(
-                'content' => $content,
-                'title' => Conf\ConfWrapper::value('site_name'),
-                'keywords' => '',
-                'description' => ''
-            )
+            'layouts/layout.admin_login.tpl.php'
         );
     }
 }
