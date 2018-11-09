6cc2486106afdcbd1942e7319d5f0594847d63ae
skulkov <sergey.kulkov@gmail.com>
2018-11-09 18:05:46 +0300

    sql dump

--- dump.sql
+++ dump.sql
@@ -12,6 +12,18 @@ CREATE TABLE `admin_log` (
   KEY `entity_id` (`entity_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `logger_entry` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at_ts` int(11) NOT NULL DEFAULT '0',
+  `user_full_id` varchar(255) DEFAULT NULL,
+  `object_full_id` varchar(255) NOT NULL,
+  `serialized_object` text,
+  `user_ip` varchar(255) DEFAULT NULL,
+  `comment` text,
+  PRIMARY KEY (`id`),
+  KEY `object_full_id` (`object_full_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `redirect_rewrites` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `src` varchar(255) NOT NULL,
