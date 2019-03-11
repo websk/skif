e915c5e763aa4713c426e37964f0fdd016c596bd
skulkov <sergey.kulkov@gmail.com>
2019-03-11 17:38:35 +0300

    fixs

--- src/WebSK/Skif/SiteMenu/SiteMenuRender.php
+++ src/WebSK/Skif/SiteMenu/SiteMenuRender.php
@@ -11,7 +11,12 @@ use WebSK\Views\ViewsPath;
  */
 class SiteMenuRender
 {
-    public static function renderSiteMenu($site_menu_id, $template = '')
+    /**
+     * @param int $site_menu_id
+     * @param string $template
+     * @return string
+     */
+    public static function renderSiteMenu(int $site_menu_id, string $template = '')
     {
         if (!$template) {
             $template_file = 'site_menu_' . $site_menu_id . '.tpl.php';
@@ -33,25 +38,29 @@ class SiteMenuRender
         return PhpRender::renderTemplate($template, array('site_menu_id' => $site_menu_id));
     }
 
-    public static function renderSiteSubMenu($site_menu_id, $parent_item_id, $template = '')
+    /**
+     * @param int $site_menu_id
+     * @param int $parent_item_id
+     * @param string $template
+     * @return string
+     */
+    public static function renderSiteSubMenu(int $site_menu_id, int $parent_item_id, string $template = '')
     {
         if (!$template) {
-            $template_file = 'site_menu_' . $site_menu_id . '_sub.tpl.php';
+            $template = 'site_menu_' . $site_menu_id . '_sub.tpl.php';
+        }
 
-            if (!ViewsPath::existsTemplateByModuleRelativeToRootSitePath('WebSK/Skif/SiteMenu', $template_file)) {
-                return PhpRender::renderTemplateInViewsDir(
-                    'site_sub_menu_default.tpl.php',
-                    array('parent_item_id' => $parent_item_id)
-                );
-            }
-
-            return PhpRender::renderTemplateForModuleNamespace(
-                'WebSK/Skif/SiteMenu',
-                $template_file,
-                array('parent_item_id' => $parent_item_id)
+        if (!ViewsPath::existsTemplateByModuleRelativeToRootSitePath('WebSK/Skif/SiteMenu', $template)) {
+            return PhpRender::renderTemplateInViewsDir(
+                'site_sub_menu_default.tpl.php',
+                ['parent_item_id' => $parent_item_id]
             );
         }
 
-        return PhpRender::renderTemplate($template, array('parent_item_id' => $parent_item_id));
+        return PhpRender::renderTemplateForModuleNamespace(
+            'WebSK/Skif/SiteMenu',
+            $template,
+            ['parent_item_id' => $parent_item_id]
+        );
     }
 }
